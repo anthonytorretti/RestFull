9e67fc6a69d0ccb638f3ce63f975dfa2e4f26605
Anthony Daniel Torretti <anthony.torretti@gmail.com>
2017-05-21 11:45:50 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -56,6 +56,6 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'test');
 define('DB_USER', 'root');
-define('DB_PASS', 'cippalippa');
+define('DB_PASS', 'password');
 define('DB_CHARSET', 'utf8');
 
